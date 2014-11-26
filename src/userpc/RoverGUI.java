0f8d3e0d80de1c428d5e4c8265a757f781948f5a
MatTaNg <ng5280@hotmail.com>
2014-11-26 12:00:22 -0800

    Changed the layout a bit, fixed the cameras, added system information

--- src/userpc/RoverGUI.java
+++ src/userpc/RoverGUI.java
@@ -1,5 +1,5 @@
 
-package userpc;
+package rover.gui;
 import javax.swing.JOptionPane;
 import javax.swing.*;
 import java.awt.*;
@@ -8,12 +8,17 @@ import java.awt.event.*;
  *
  * @author Matt
  */
-public class RoverGUI extends javax.swing.JFrame{
+public class RoverGUI {
 
     private JFrame f; //The window
     private JPanel p; //Stuff inside the window(Its a div essentially)
     private JPanel pCameras;
     private JPanel pPanCamera;
+    
+    private JLabel camera1;
+    private JLabel camera2;
+    private JLabel systemData;
+    
     private JButton connect;
     private JButton disConnect;
     private JButton cameraR;
@@ -33,10 +38,6 @@ public class RoverGUI extends javax.swing.JFrame{
     public static void main(String[] args) {
         
         new RoverGUI();
-       /* String fn = JOptionPane.showInputDialog("Enter first number"); //Entered number is stored in string fn.
-        
-        JOptionPane.showMessageDialog(null, "The answer is " + fn, "The title",""); //p(position on screen, null = center, message, title bar)
-        // TODO code application logic here*/
     }
     
     public void gui() {
@@ -50,20 +51,28 @@ public class RoverGUI extends javax.swing.JFrame{
         p = new JPanel(new GridBagLayout());
         pCameras = new JPanel(new GridBagLayout());
         pPanCamera = new JPanel(new GridBagLayout());
-        pCameras = new JPanel(new GridBagLayout());
-        pCameras.setBackground(Color.YELLOW);
         
         GridBagConstraints c = new GridBagConstraints();//GridBagConstraint is a specific way of arranging the objects in java.
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.VERTICAL;
         
         connect = new JButton("Connect");
         disConnect = new JButton("Disconnect");
         cameraR = new JButton(">>");
         cameraL = new JButton("<<");
+        lab = new JLabel("Status: " + connected);
+        camera1 = new JLabel("Camera 1");
+        camera1.setBackground(Color.red);
+        camera1.setOpaque(true); //Make the background color show up.
+        camera2= new JLabel("Camera 2");
+        camera2.setBackground(Color.red);
+        camera2.setOpaque(true);
+        systemData = new JLabel("System Information..."); //Change the string here for system information.
         
+       
         connect.addActionListener(new ActionListener() {
             
             public void actionPerformed(ActionEvent e) {
-                //JOptionPane.showMessageDialog(null, "connect");
                 connected = "Active";
                 lab.setText("Status: " + connected);
             }
@@ -93,34 +102,57 @@ public class RoverGUI extends javax.swing.JFrame{
             }
         });
         
-       lab = new JLabel("Status: " + connected);
+
                 
         //Add button & label to panel; add panel to frame.
         c.insets = new Insets(10,10,10,10);//makes buttons have margins(top, left, right, bottom)
         
-        c.gridx = 0; //Set the x axis for the object
-        c.gridy = 2; //Set the y axis for the object
-        p.add(disConnect, c);
+        c.gridx = 0;
+        c.gridy = 0;
+        p.add(systemData, c);
         
-        c.gridx = 0; //Set the x axis for the object
-        c.gridy = 1; //Set the y axis for the object
+        c.gridx = 1; //Set the x axis for the object
+        c.gridy = 0; //Set the y axis for the object
         p.add(connect, c);
+        
+        c.gridx = 2; //Set the x axis for the object
+        c.gridy = 0; //Set the y axis for the object
+        p.add(disConnect, c);
         
         c.gridx = 0; //Set the x axis for the object
         c.gridy = 0; //Set the y axis for the object
         pPanCamera.add(cameraL, c);
         
-        c.gridx = 10; //Set the x axis for the object
+        c.gridx = 1; //Set the x axis for the object
         c.gridy = 0; //Set the y axis for the object
         pPanCamera.add(cameraR, c);
         
-        c.gridx = 0;
-        c.gridy = 5;
+        c.gridx = 3;
+        c.gridy = 0;
         p.add(lab, c);
                         
-        f.add(p,BorderLayout.WEST);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.fill = GridBagConstraints.BOTH;
+
+        pCameras.add(camera1, c);
+                
+        c.gridx = 2;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.fill = GridBagConstraints.BOTH;
+        
+        pCameras.add(camera2, c);
+
+
+        f.add(p,BorderLayout.NORTH);
         f.add(pPanCamera, BorderLayout.SOUTH);
         f.add(pCameras, BorderLayout.CENTER);
+
+
     }
     
 }
