464ae2f54290abf3b6ceda11d25cc46058c39182
junxinhe <hej77@students.rowan.edu>
2014-12-05 15:26:02 -0500

    Thread Communication

--- src/communication/Server.java
+++ src/communication/Server.java
@@ -5,13 +5,7 @@
  */
 package communication;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -31,10 +25,9 @@ public class Server implements Runnable {
     RoverGUI gui;
     MotorControllers mc;
     ArmControllers ac;
-    Socket socket1 = null;
-    Socket socket2 = null;
+    Socket socket = null;
     ServerSocket listener = null;
-    private Thread thread = null;
+    private PrintWriter out;
 
     public Server(RoverGUI gui, MotorControllers mc, ArmControllers ac) {
         this.gui = gui;
@@ -50,46 +43,18 @@ public class Server implements Runnable {
     @Override
     public void run() {
         try {
-            socket1 = listener.accept();
-            gui.display("Client 1 accept");
-            socket2 = listener.accept();
-            gui.display("Client 2 accept");
+            socket = listener.accept();
+            gui.display("Client accept");
+            out =new PrintWriter(socket.getOutputStream(), true);
+            while(true){
+                out.println(mc.getMotorData());
+                out.println(ac.getArmData());
+                Thread.sleep(100);
+            }
         } catch (IOException ex) {
             gui.display("Unable to accept client!");
-            return;
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    class Client extends Thread {
-
-        Socket socket;
-        private ObjectInputStream in;
-        private ObjectOutputStream out;
-
-        public Client(Socket socket) {
-            this.socket = socket;
-        }
-
-        @Override
-        public void run() {
-            while (thread != null) {
-                //out.println(mc.getMotorData());
-                //out.println(ac.getArmData());
-                try {
-                    in = new ObjectInputStream(socket.getInputStream());
-                    out = new ObjectOutputStream(socket.getOutputStream());
-                    gui.display(in.readObject().toString());
-                    out.writeObject("hi");
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (ClassNotFoundException ex) {
-                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }
-    }
-
 }
